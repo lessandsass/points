8bd08289cec9cbee141f4402aa455e02d14f3485
Less <31947204+lessandsass@users.noreply.github.com>
2020-10-16 21:37:17 +0530

    authentication part is completed for points projects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,38 @@
 <body class="bg-gray-100">
 
     <nav class="p-6 bg-white flex justify-between">
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Dashboard</a></li>
+
+        <ul class="flex items-center">
+            <li><a href="" class="p-3">Home</a></li>
+            <li><a href="{{ route('dashboard') }}" class="p-3">Dashboard</a></li>
         </ul>
+
+        <ul class="flex items-center">
+            @auth
+                <li><a href="" class="p-3">{{ auth()->user()->name }}</a></li>
+                <li>
+                    <form action="{{ route('logout') }}" method="post" class="p-3 inline">
+                        @csrf
+                        <button type="submit">Logout</button>
+                    </form>
+                </li>
+            @endauth
+
+            @guest
+                <li><a href="{{ route('login') }}" class="p-3">Login</a></li>
+                <li><a href="{{ route('register') }}" class="p-3">Register</a></li>
+            @endguest
+            
+
+            
+
+        </ul>
+
     </nav>
 
-    @yield('content')
+    <div class="container mx-auto mt-6 px-6">
+        @yield('content')   
+    </div>
+    
 </body>
 </html>
